6c4705c093148234e72a715cad01f4597e8370fa
huybq886 <huybuiquang886@gmail.com>
2021-02-01 14:38:37 +0700

    haizzz

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.perform_deliveries = true
+config.action_mailer.smtp_settings = {
+  domain: 'damp-falls-29791.herokuapp.com',
+  address:        "smtp.sendgrid.net",
+  port:            587,
+  authentication: :plain,
+  user_name:      'apikey',
+  password:       ENV['SENDGRID_API_KEY']
+}
