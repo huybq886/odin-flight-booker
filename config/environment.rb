d54add2b6ad1bbfed535652e5b9b491dbbc67b3a
huybq886 <huybuiquang886@gmail.com>
2021-02-01 15:08:20 +0700

    haizzzzzz

--- config/environment.rb
+++ config/environment.rb
@@ -4,11 +4,14 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-config.action_mailer.smtp_settings = {
-  :user_name => 'apikey',
-  :password => ENV['SENDGRID_API_KEY'],
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
+
+Rails.application.configure do
+  config.action_mailer.smtp_settings = {
+    :user_name => 'apikey',
+    :password => ENV['SENDGRID_API_KEY'],
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+end
